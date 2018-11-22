8bca7d215b817f39c2f130ceb810936df613dc7f
Maksym-Gorbunov <maxsverige@hotmail.com>
2018-11-22 14:16:31 +0000

    static usage() in GuessinGame was created

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,7 +1,16 @@
 public class GuessingGame {
     public static void main(String[] args) {
-        Guesser guesser = new Guesser(0, 1000);
+        int low = Integer.parseInt(args[0]);
+        int high = Integer.parseInt(args[1]);
+        Guesser guesser = new Guesser(low, high);
         guesser.start();
+    }
+
+    public static void  usage(){
+        System.out.println("Usage: java GuessingGame low high\n"+
+                            "      where low is an integer for the lower bound\n"+
+                            "      and high is an integer for the upper bound\n"+
+                            "      of the interval for the numbers of the GuessingGame");
     }
 }
 
